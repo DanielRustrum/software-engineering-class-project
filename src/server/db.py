d42238205d78883e280fb9f23211942d8e42418f
Daniel Rustrum <danieltrustrum@gmail.com>
2019-04-13 07:38:24 -0700

    DB Changes

--- src/server/db.py
+++ src/server/db.py
@@ -2,25 +2,72 @@ import pyrebase
 
 from settings import settingsManager
 
-firebase = pyrebase.initialize_app(settingsManager.getSettings["dbkeys"])
+database = pyrebase.initialize_app(settingsManager.getSettings["dbkeys"]).database()
 
+
+#* Auth
+def updateToken(uid):
+    pass
+
+def getToken(uid):
+    pass
+
+def modifyPassword(uid):
+    pass
+
+def getPassword(uid):
+    pass    
 
 #* Users
-def createUser(data):
-    pass
+def createUser(uid, data):
+    userData = {
+        "settings": data["settings"],
+        "username": data["username"]
+    }
+    authData = {
+        "pass": data["password"],
+        "token": "none"
+    }
 
-def deleteUser(id):
-    pass
+    database.child("users").child(uid).push(userData)
+    database.child("auth").child(uid).push(authData)
 
-def 
+def deleteUser(uid):
+    database.child("users").child(uid).set(None)
+    database.child("auth").child(uid).set(None)
+
+def getSettings(uid):
+    return database.child("users").child(uid).child("settings")
+
+def modifySettings(uid, settings):
+    database.child("users").child(uid).child("settings").set(settings)
+
+def modifyUsername(uid, username):
+    database.child("users").child(uid).child("username").set(username)
+
+def getUsername(uid):
+    return database.child("")
 
 
 #* Entries
-def createEntry(id):
+def createEntry(data):
     pass
 
-def modifyEntry(id):
+def modifyEntry(eid):
     pass
 
-def deleteEntry(id):
+def deleteEntry(eid):
+    pass
+
+#* Tags
+def createTag(data):
+    pass
+
+def deleteTag(tid):
+    pass
+
+def addTag(tid, eid):
+    pass
+
+def removeTag(tid, eid):
     pass
